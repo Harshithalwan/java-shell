7fb628277d76e138a1e44413baab3ba63cba1051
harshithalwan-turing <harshit.h@turing.com>
2026-06-04 00:29:43 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,41 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
          Scanner scanner = new Scanner(System.in);
+         enum COMMANDS {
+             EXIT("exit"),
+             ECHO("echo"),
+             TYPE("type");
 
+             private final String value;
+             COMMANDS(String s) {
+                 this.value = s;
+             }
+             public String getValue(){
+                 return value;
+             }
 
+             public static boolean containsValue(String name) {
+                 return Arrays.stream(values()).anyMatch(enumKey -> enumKey.getValue().equals(name));
+             }
+         }
          while(true){
              System.out.print("$ ");
              String input = scanner.nextLine();
-             if(input.equals("exit")){
+             if(input.equals(COMMANDS.EXIT.getValue())){
                  break;
              }
-             else if(input.startsWith("echo ")){
+             else if(input.startsWith(COMMANDS.ECHO.getValue() + " ")){
                  System.out.println(input.substring(5));
+             }
+             else if(input.startsWith(COMMANDS.TYPE.getValue() + " ")){
+                 String trimmedInput = input.substring(5);
+                 if(COMMANDS.containsValue(trimmedInput)) {
+                     System.out.println(trimmedInput + " is a shell builtin");
+                 }
              }
              else{
                  System.out.println(input + ": command not found");
