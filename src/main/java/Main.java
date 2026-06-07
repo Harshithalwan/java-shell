8e0a2790a4de5ca58bdcd3edfdf06a50f40d9984
harshithalwan-turing <harshit.h@turing.com>
2026-06-08 00:47:55 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,33 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+
+    private static final String PATH = "PATH";
+
+    enum COMMANDS {
+        EXIT("exit"),
+        ECHO("echo"),
+        TYPE("type");
+
+        private final String value;
+        COMMANDS(String s) {
+            this.value = s;
+        }
+        public String getValue(){
+            return value;
+        }
+
+        public static boolean containsValue(String name) {
+            return Arrays.stream(values()).anyMatch(enumKey -> enumKey.getValue().equals(name));
+        }
+    }
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
          Scanner scanner = new Scanner(System.in);
-         enum COMMANDS {
-             EXIT("exit"),
-             ECHO("echo"),
-             TYPE("type");
-
-             private final String value;
-             COMMANDS(String s) {
-                 this.value = s;
-             }
-             public String getValue(){
-                 return value;
-             }
-
-             public static boolean containsValue(String name) {
-                 return Arrays.stream(values()).anyMatch(enumKey -> enumKey.getValue().equals(name));
-             }
-         }
          while(true){
              System.out.print("$ ");
              String input = scanner.nextLine();
@@ -36,7 +42,18 @@ public class Main {
                  if(COMMANDS.containsValue(trimmedInput)) {
                      System.out.println(trimmedInput + " is a shell builtin");
                  } else {
-                     System.out.println(trimmedInput + ": not found");
+                     String dirList = System.getenv(PATH);
+                     boolean pathFound = false;
+                     Path path = null;
+                     for(String dir : dirList.split(File.pathSeparator)){
+                         path = Path.of(dir, trimmedInput);
+                         if(Files.isExecutable(path)){
+                             pathFound = true;
+                             break;
+                         }
+                     }
+                     if(pathFound) System.out.println(trimmedInput + " is " + path);
+                     else System.out.println(trimmedInput + ": not found");
                  }
              }
              else{
